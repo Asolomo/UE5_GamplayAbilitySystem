b615de2d7fc4056eb1cb3bbca5b20ba5ad637518
Adnan Solomon <adnansolomon589@gmail.com>
2024-11-02 02:12:08 -0400

    ABP's added and input started

---
+++ Source/Aura/Private/Character/InputController.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "Character/InputController.h"
+
---
+++ Source/Aura/Public/Character/InputController.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "InputController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AInputController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
