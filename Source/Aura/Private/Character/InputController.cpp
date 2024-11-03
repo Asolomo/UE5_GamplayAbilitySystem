c33fbcd51d67cc7f3dafb815bb9f09b35db7e231
Adnan Solomon <adnansolomon589@gmail.com>
2024-11-03 11:29:21 -0500

    started input controller stuff

--- Source/Aura/Private/Character/InputController.cpp
+++ Source/Aura/Private/Character/InputController.cpp
@@ -2,4 +2,18 @@
 
 
 #include "Character/InputController.h"
+#include "EnhancedInputSubsystems.h"
 
+AInputController::AInputController(){
+	bReplicates = true;
+}
+
+void AInputController::BeginPlay(){
+	Super::BeginPlay();
+	check(PlayerContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(PlayerContext, 0);
+	
+}
--- Source/Aura/Public/Character/InputController.h
+++ Source/Aura/Public/Character/InputController.h
@@ -6,6 +6,7 @@
 #include "GameFramework/PlayerController.h"
 #include "InputController.generated.h"
 
+class UInputMappingContext;
 /**
  * 
  */
@@ -13,5 +14,12 @@ UCLASS()
 class AURA_API AInputController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+public:
+	AInputController();
+protected:
+	virtual void BeginPlay() override;
+private:
+	UPROPERTY(EditAnywhere, Category= "Input")
+	TObjectPtr<UInputMappingContext> PlayerContext;
 };
